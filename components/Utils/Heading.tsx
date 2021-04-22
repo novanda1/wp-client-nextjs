e1db40c6263f07ddba09e3df67f7169378a76e79
Novanda Ahsan <nathoilla@indo-soft.com>
2021-04-22 10:02:27 +0700

    fix tw props on Utils/Header

--- components/Utils/Heading.tsx
+++ components/Utils/Heading.tsx
@@ -1,18 +1,15 @@
-import tw from "twin.macro";
 import { styled } from "../../stitches.config";
-
-const tww = tw;
 
 const HeadingSt = styled("h1", {});
 
-const Box: React.FC<{ css?: string; tw?: string; as?: any }> = ({
+const Box: React.FC<{ css?: string; tw?: object; as?: any }> = ({
   children,
   css,
   tw,
   as,
 }) => {
   return (
-    <HeadingSt css={{ css, ...tww`${tw}` }} as={as}>
+    <HeadingSt css={{ css, ...tw }} as={as}>
       {children}
     </HeadingSt>
   );
