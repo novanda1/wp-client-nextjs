a1d290c9db30611487b1254e422258213c02b10e
Novanda Ahsan <nathoilla@indo-soft.com>
2021-04-27 15:11:29 +0700

    fix css on header

--- src/components/Partials/Header.tsx
+++ src/components/Partials/Header.tsx
@@ -1,19 +1,23 @@
+import Link from 'next/link';
+import React from 'react';
 import tw from 'twin.macro';
 import { styled } from '../../../stitches.config';
-import Link from 'next/link';
 import { SITE_NAME } from '../../constants';
-import React from 'react';
 
 const HeaderHeading = styled(
     'h2',
     tw`text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8`,
 );
 
+const LinkSt = styled('a', {
+    ...tw`hover:underline cursor-pointer`,
+});
+
 const Header: React.FC = () => {
     return (
         <HeaderHeading>
             <Link href="/">
-                <a tw="hover:underline">{SITE_NAME}</a>
+                <LinkSt>{SITE_NAME}</LinkSt>
             </Link>
             .
         </HeaderHeading>
