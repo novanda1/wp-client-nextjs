8603f874f2eec2f049153d9b59f6ff216708fd8e
Novanda Ahsan <nathoilla@indo-soft.com>
2021-05-03 10:16:14 +0700

    run prettier

--- stitches.config.ts
+++ stitches.config.ts
@@ -1,15 +1,8 @@
-import { createCss } from '@stitches/react'
+import { createCss } from '@stitches/react';
 
-export const {
-    css,
-    styled,
-    global,
-    theme,
-    keyframes,
-    getCssString,
-} = createCss({
+export const { css, styled, global, theme, keyframes, getCssString } = createCss({
     prefix: '',
     theme: {},
     themeMap: {},
     utils: {},
-})
+});
